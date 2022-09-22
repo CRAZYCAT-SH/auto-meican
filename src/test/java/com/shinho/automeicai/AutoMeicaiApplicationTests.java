1f8368d0bd70b4ff5ead1ff527febec8833169d5
李永炳 <liyongbing@shinho.net.cn>
2022-09-22 18:24:16 +0800

    feature:功能开发

--- src/test/java/com/shinho/automeicai/AutoMeicaiApplicationTests.java
+++ src/test/java/com/shinho/automeicai/AutoMeicaiApplicationTests.java
@@ -1,24 +1,30 @@
 package com.shinho.automeicai;
 
-import com.shinho.automeicai.dto.BaseRequest;
+import com.shinho.automeicai.dto.CalendarItemsRequest;
 import com.shinho.automeicai.remote.TokenService;
+import com.shinho.automeicai.remote.impl.CalendarItemsExecutor;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
+import java.time.LocalDate;
 
 @SpringBootTest
 class AutoMeicaiApplicationTests {
 
     @Resource
     private TokenService tokenService;
+    @Resource
+    private CalendarItemsExecutor calendarItemsExecutor;
 
     @Test
-    void contextLoads() {
-        BaseRequest param = new BaseRequest();
+    void test() {
+        CalendarItemsRequest param = new CalendarItemsRequest();
         param.setUsername("18762036036@shinho.net.cn");
         param.setPassword("Shinho123");
-        System.out.println(tokenService.getToken(param));
+        param.setBeginDate(LocalDate.now().toString());
+        param.setEndDate(LocalDate.now().toString());
+        System.out.println(calendarItemsExecutor.execute(param));
     }
 
 }
